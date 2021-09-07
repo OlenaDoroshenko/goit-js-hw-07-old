b76407212846789c5b916b33301351c8971f0d7e
OlenaDoroshenko <70623973+OlenaDoroshenko@users.noreply.github.com>
2021-09-07 23:54:44 +0300

    remove js

--- js/task4.js
+++
@@ -1,8 +0,0 @@
-import users from "./users.js";
-// Получить массив только неактивных пользователей (поле isActive).
-
-const getInactiveUsers = users => {
-  return users.filter(user =>!user.isActive);
-};
-
-console.log(getInactiveUsers(users)); // [объект Moore Hensley, объект Ross Vazquez, объект Blackburn Dotson]
--- js/task7.js
+++
@@ -1,8 +0,0 @@
-import users from "./users.js";
-// Получить общую сумму баланса (поле balance) всех пользователей.
-
-const calculateTotalBalance = users => {
-return users.reduce((acc,user) => acc + user.balance, 0,);
-};
-
-console.log(calculateTotalBalance(users)); // 20916
